cdfdca1606e72e35554c0e64b646acd54abb8f2b
nayon-sd <nayonsd6031@gmail.com>
2022-02-08 01:58:48 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// wow-animation-add
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+
+
+  // owl-carousel-add
+  $(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+  });
